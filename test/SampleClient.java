f694b59ff196f20f16d0ef11a43b0b47e7d858d1
David Hodgins <david.hodgins@mthreeconsulting.com>
2017-03-08 15:44:38 +0000

    fix issue where we only tried to listen once for message by sitting in an infinite read loop add logging rewrite is available logic to new way of doing it, now we know that creating input/output streams sends headers across the wire

--- test/SampleClient.java
+++ test/SampleClient.java
@@ -73,38 +73,41 @@ public class SampleClient extends Mock implements Client{
 		
 		ObjectInputStream is;
 		try {
-			is = new ObjectInputStream(omConn.getInputStream());
-			//is.wait(); //this throws an exception!!
-			while(0<is.available()){
-				String fix=(String)is.readObject();
-				String[] fixTags=fix.split(";");
-				int OrderId=-1;
-				char MsgType;
-				int OrdStatus;
-				methods whatToDo=methods.dontKnow;
-				//String[][] fixTagsValues=new String[fixTags.length][2];
-				for(int i=0;i<fixTags.length;i++){
-					String[] tag_value=fixTags[i].split("=");
-					switch(tag_value[0]){
-						case"11":OrderId=Integer.parseInt(tag_value[1]);break;
-						case"35":MsgType=tag_value[1].charAt(0);
-							if(MsgType=='A')whatToDo=methods.newOrderSingleAcknowledgement;
-							break;
-						case"39":OrdStatus=tag_value[1].charAt(0);break;
+			while(true){
+				//is.wait(); //this throws an exception!!
+				while(0<omConn.getInputStream().available()){
+					is = new ObjectInputStream(omConn.getInputStream());
+					String fix=(String)is.readObject();
+					System.out.println(Thread.currentThread().getName()+" received fix message: "+fix);
+					String[] fixTags=fix.split(";");
+					int OrderId=-1;
+					char MsgType;
+					int OrdStatus;
+					methods whatToDo=methods.dontKnow;
+					//String[][] fixTagsValues=new String[fixTags.length][2];
+					for(int i=0;i<fixTags.length;i++){
+						String[] tag_value=fixTags[i].split("=");
+						switch(tag_value[0]){
+							case"11":OrderId=Integer.parseInt(tag_value[1]);break;
+							case"35":MsgType=tag_value[1].charAt(0);
+								if(MsgType=='A')whatToDo=methods.newOrderSingleAcknowledgement;
+								break;
+							case"39":OrdStatus=tag_value[1].charAt(0);break;
+						}
 					}
+					switch(whatToDo){
+						case newOrderSingleAcknowledgement:newOrderSingleAcknowledgement(OrderId);
+					}
+					
+					/*message=connection.getMessage();
+					char type;
+					switch(type){
+						case 'C':cancelled(message);break;
+						case 'P':partialFill(message);break;
+						case 'F':fullyFilled(message);
+					}*/
+					show("");
 				}
-				switch(whatToDo){
-					case newOrderSingleAcknowledgement:newOrderSingleAcknowledgement(OrderId);
-				}
-				
-				/*message=connection.getMessage();
-				char type;
-				switch(type){
-					case 'C':cancelled(message);break;
-					case 'P':partialFill(message);break;
-					case 'F':fullyFilled(message);
-				}*/
-				show("");
 			}
 		} catch (IOException|ClassNotFoundException e){
 			// TODO Auto-generated catch block
@@ -113,6 +116,7 @@ public class SampleClient extends Mock implements Client{
 	}
 
 	void newOrderSingleAcknowledgement(int OrderId){
+		System.out.println(Thread.currentThread().getName()+" called newOrderSingleAcknowledgement");
 		//do nothing, as not recording so much state in the NOS class at present
 	}
 /*listen for connections
